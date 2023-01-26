27efa2bf11d7c5d495ce0e7985c706f6035b6d20
Davi Albino <negroalbino_@hotmail.com>
2023-01-26 18:35:01 -0300

    container backend não está funcionando

--- app/backend/src/database/migrations/1-create-users.js
+++ app/backend/src/database/migrations/1-create-users.js
@@ -15,6 +15,9 @@ module.exports = {
             },
             password: {
                 type: Sequelize.STRING
+            },
+            role: {
+                type: Sequelize.STRING
             }
         })
     },
