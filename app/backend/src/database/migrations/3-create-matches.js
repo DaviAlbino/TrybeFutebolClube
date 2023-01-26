f504e3ec940df01d4ea929edcbea590572d42da3
Davi Albino <negroalbino_@hotmail.com>
2023-01-26 17:49:51 -0300

    Migrations and Models

---
+++ app/backend/src/database/migrations/3-create-matches.js
@@ -0,0 +1,36 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('matches', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            home_team_goals: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            home_team_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            away_team_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            away_team_goals: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            in_progress: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false
+            }
+        });
+    },
+
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('matches');
+    }
+}
