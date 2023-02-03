6ca5a931491a45e3f8c5ec02301f7512a21b6fb5
Davi Albino <negroalbino_@hotmail.com>
2023-02-03 16:08:54 -0300

    post/login

--- app/backend/src/routes/UserRoutes.ts
+++ app/backend/src/routes/UserRoutes.ts
@@ -1,13 +1,11 @@
 import * as express from 'express';
 import UserController from '../controllers/user.controller';
 
-const userController = new UserController();
-
 export default class LoginRouter {
   public router: express.Router;
 
   constructor() {
     this.router = express.Router();
-    this.router.post('/', userController);
+    this.router.post('/', UserController.login);
   }
 }
