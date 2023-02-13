61da5f179f80cc735df9dd0175c285916a4acaba
Davi Albino <negroalbino_@hotmail.com>
2023-02-13 10:26:39 -0300

    feat: creating Away board util

--- app/backend/src/interfaces/IAwayBoard.ts
+++ app/backend/src/interfaces/IAwayBoard.ts
@@ -2,5 +2,5 @@ import IMatchGoals from './IMatchGoals';
 
 export default interface IAwayBoard {
   teamName: string;
-  homeMatches: IMatchGoals[];
+  awayMatches: IMatchGoals[];
 }
---
+++ app/backend/src/utils/SetLeaderboardAway.util.ts
@@ -0,0 +1,22 @@
+import ILeaderboard from '../interfaces/ILeaderboard';
+import IAwayBoard from '../interfaces/IAwayBoard';
+import SetLeaderboardRules from './SetLeaderboardRules.util';
+
+export default class SetLeaderboardHome {
+  static createHomeLeaderboard = (homeTeams: IAwayBoard[]): ILeaderboard[] => {
+    const board = homeTeams.map(({ teamName, awayMatches }) => ({
+      name: teamName,
+      totalPoints: SetLeaderboardRules.calculatePoints(awayMatches),
+      totalGames: SetLeaderboardRules.countGames(awayMatches),
+      totalVictories: SetLeaderboardRules.awayVictory(awayMatches),
+      totalLosses: SetLeaderboardRules.homeVictory(awayMatches),
+      totalDraws: SetLeaderboardRules.draw(awayMatches),
+      goalsFavor: SetLeaderboardRules.totalAwayGoals(awayMatches),
+      goalsOwn: SetLeaderboardRules.totalHomeGoals(awayMatches),
+      goalsBalance: SetLeaderboardRules.goalsBalance(awayMatches),
+      efficiency: Number(SetLeaderboardRules.setEfficiency(awayMatches)),
+    }));
+
+    return board;
+  };
+}
